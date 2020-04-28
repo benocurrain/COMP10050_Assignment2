0bb93d23b23345b0a9cdf06df3e7263ee3eeff68
benocurrain <63253449+benocurrain@users.noreply.github.com>
2020-04-28 20:55:49 +0100

    Moves.c

--- Assignment_2/Move.c
+++ Assignment_2/Move.c
@@ -10,13 +10,13 @@ void Move(int Player){
     int dom_plyer_bfr, dom_other_bfr, dom_player_aft, dom_other_aft ; // Used to calculate the change in dominated stacks after a move
     int X_Co, Y_Co; // The co-ordinates from which the user will move pieces to
     int right_player = 0 ; // Is used to check if the stack chosen by player is dominated by them
-    int no_to_move=0,no_spaces = 0 ; // The number of pieces the user will move from selected stack, and the number os spaces they will move it
+    int no_to_move=1,no_spaces = 1 ; // The number of pieces the user will move from selected stack, and the number os spaces they will move it
     int direction ; // Used for selecting the direction the user will move
     int total_X=0, total_y=0; // X will be distance left/right, y will be up/down
     int new_X, new_Y ; // Will be co-ordinates of the stack the user is moving to
     int i ;
 
-    printf("\nIt is player %d's turn.\n ") ;
+    printf("\nIt is player %d's turn.\n ",Player) ;
 
     if(Player==1 &&  Pl1_piece>0){
         printf("Would you like to\n1.Move a stack on the board\n2.Place a piece from your hand on the board?") ;
@@ -30,11 +30,7 @@ void Move(int Player){
     if(player_choice==1) { // Move stack
         // Getting the stack user would like to move
         do {
-            printf("What position would you like to move from\n");
-            printf("Please enter an X co-ordinate:");
-            scanf("%d", &X_Co);
-            printf("Please enter a Y co-ordinate:");
-            scanf("%d", &Y_Co);
+            get_Co_ordinates(Player,&Y_Co, &X_Co);
 
             if (board[Y_Co][X_Co]->no_Pieces == 0) {
                 printf("\nThere are no pieces in this position please Choose another position.\n");
@@ -46,17 +42,22 @@ void Move(int Player){
         } while (board[Y_Co][X_Co]->no_Pieces == 0 || right_player == 0);
 
         // Checking how many pieces the user would like to move from stack
-        do {
-            printf("\nHow many pieces would you like to move from stack?\nThere are %d available to move:",
-                   board[Y_Co][X_Co]->no_Pieces);
-            scanf("%d", &no_to_move);
-        } while ((no_to_move < 1) || (no_to_move > board[Y_Co][X_Co]->no_Pieces));
+        if(board[Y_Co][X_Co]->no_Pieces>1) {
+            do {
+                printf("\nHow many pieces would you like to move from stack?\nThere are %d available to move:",
+                       board[Y_Co][X_Co]->no_Pieces);
+                scanf("%d", &no_to_move);
+            } while ((no_to_move < 1) || (no_to_move > board[Y_Co][X_Co]->no_Pieces));
+        }
 
-        // Asking how many spaces the user woould like to move their stack
-        do {
-            printf("\nHow many spaces would you like to move the stack");
-            scanf("%d", &no_spaces);
-        } while ((no_spaces < 1) || (no_spaces > no_to_move));
+        // Asking how many spaces the user would like to move their stack
+        if(no_to_move>1) {
+            do {
+                printf("\nHow many spaces would you like to move the stack");
+                scanf("%d", &no_spaces);
+            } while ((no_spaces < 1) || (no_spaces > no_to_move));
+        }
+
 
         // Getting the direction the user wants to move
         for (i = 0; i < no_spaces; i++) {
@@ -84,7 +85,7 @@ void Move(int Player){
         new_X = X_Co + total_X;
         new_Y = Y_Co + total_y;
 
-        // Updating dominated stacks numbers
+        // Updating dominated stacks before move numbers
         if(board[new_Y][new_X]->Top == NULL){
             dom_plyer_bfr = 1 ;
             dom_other_bfr = 0 ;
@@ -150,7 +151,7 @@ void Move(int Player){
                 break;
         }
 
-
+        // Moving stack
         board[new_Y][new_X]->Top = board[Y_Co][X_Co]->Top;
         board[Y_Co][X_Co]->Top = tmp;
 
@@ -185,26 +186,22 @@ void Move(int Player){
 
     }
     else if (player_choice == 2){ // New Piece
-        printf("What position would you like to place your piece?\n");
-        printf("Please enter an X co-ordinate:");
-        scanf("%d", &X_Co);
-        printf("Please enter a Y co-ordinate:");
-        scanf("%d", &Y_Co);
-        printf("Running 1\n") ;
-        if(board[Y_Co][X_Co]->Top==NULL){
+        get_Co_ordinates(Player,&new_Y, &new_X);
+
+        if(board[new_Y][new_X]->Top==NULL){
             add_Dom_stck_player(Player) ;
-        } else if (board[Y_Co][X_Co]->Top->player_col != Player){
+        } else if (board[new_Y][new_X]->Top->player_col != Player){
             add_Dom_stck_player(Player) ;
             subtract_Dom_stck_other(Player);
-        }else{ printf("Youre a wamker"); }
+        }else{ printf("Error"); }
 
         struct piece *new_piece ;
         new_piece = malloc(sizeof(struct piece)) ;
 
         new_piece->player_col = Player ;
-        new_piece->piece_below = board[Y_Co][X_Co]->Top ;
-        board[Y_Co][X_Co]->Top = new_piece ;
-        board[Y_Co][X_Co]->no_Pieces++;
+        new_piece->piece_below = board[new_Y][new_X]->Top ;
+        board[new_Y][new_X]->Top = new_piece ;
+        board[new_Y][new_X]->no_Pieces++;
 
         if(Player==1) {
             Pl1_piece--;
@@ -255,7 +252,7 @@ void get_Co_ordinates(int player, int *row, int *column ){
     printf("What position would you like to move from\n");
 
     do {
-        printf("\nPlease select a Row");
+        printf("\nPlease select a Row:");
         scanf("%d", &transfer_row);
 
         if(transfer_row<1 || transfer_row>6)
@@ -273,7 +270,7 @@ void get_Co_ordinates(int player, int *row, int *column ){
     }while(transfer_column<1 || transfer_column>6);
 
     *row = transfer_row ;
-    *column = transfer_column
+    *column = transfer_column ;
 }
 
 void add_Dom_stck_player(int plyer){
